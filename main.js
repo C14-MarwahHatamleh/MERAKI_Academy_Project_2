f993e385f1a4ee7ac4ccdd61f5f11167a5983a2f
MarwahHatamleh <marwahatamleh98@gmail.com>
2025-03-11 09:49:51 +0300

    type html and css

---
+++ main.js
@@ -0,0 +1,170 @@
+const Recipes = {
+  header: "Welcome to our Quiz",
+  Paragraph:
+    "Hello Friend! Welcome to a world of endless possibilities. Explore your Knowledge in our Quiz in the different categoreies. of products and services crafted to enrich your life. Prepare yourself and Take the Quiz for progress progress and improve your knowledge. We are here to assist you at every stage of your journey.",
+  imgSrc: "Portrait-of-a-cat-with-whiskers-visible.webp",
+  categories: [
+    {
+      Spaghetti: {
+        plates: [
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+        ],
+      },
+    },
+    {
+      Salads: {
+        plates: [
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+        ],
+      },
+    },
+    {
+      chicken_dip: {
+        questions: [
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+          {
+            title: "dvd",
+            Image: "dsvd",
+            Description: "vdv",
+            Rate: "dvds",
+          },
+        ],
+      },
+    },
+  ],
+};
